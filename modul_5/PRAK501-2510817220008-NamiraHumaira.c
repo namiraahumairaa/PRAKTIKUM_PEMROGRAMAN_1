7057c79d563a7097cfa9a0a7ce45ee2658b86fe3
namieeee <namiraahumairaa@gmail.com>
2025-12-04 10:32:23 +0800

    Create PRAK501-2510817220008-NamiraHumaira.c

---
+++ modul_5/PRAK501-2510817220008-NamiraHumaira.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int MaxBilangan(int a, int b, int c, int d)
+{
+    int maksimal = a;
+    if(b > maksimal) 
+    maksimal = b;
+    if (c > maksimal) 
+    maksimal = c;
+    if (d > maksimal) 
+    maksimal = d;
+    return maksimal;
+} 
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int hasil = MaxBilangan(a, b, c, d);
+    printf("%d", hasil);
+    return 0;
+}
